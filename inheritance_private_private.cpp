bad15ed61ab4e9d8bab0f7b7e7e79f0ad1a9208b
Om Yadav <159103737+omyadav11@users.noreply.github.com>
2024-05-06 15:18:57 +0530

    Add files via upload
    inhiritance

---
+++ inheritance_private_private.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Person{
+	private:
+	int age;
+	long int mob;
+	string name;
+	string add;
+	
+	void input(){
+		cout<<"enter your information"<<endl<<"1.name"<<endl<<"2.age"<<endl<<"3.mob"<<endl<<"4.add"<<endl;
+		cin>>name>>age>>mob>>add;
+		
+	}
+	void display(){
+		cout<<name<<endl<<age<<endl<<mob<<endl<<add<<endl;	
+	}
+
+};
+
+class Student:private Person{
+	public:
+	int prn;
+	int marks;
+	string div;
+	void get(){
+		input();
+		cout<<"enter prn,marks and div"<<endl;
+		cin>>prn>>marks>>div;
+	}
+	void print(){
+		display();
+		cout<<prn<<endl<<marks<<endl<<div<<endl;
+	}
+};
+
+int main(){
+	Student s1;
+	s1.get();
+	s1.print();
+	//s1.input();		
+
+
+}
+
